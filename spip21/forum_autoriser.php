a0cc97c2f1c0058e5d12fce1b12ae0632644a639
kent1 <kent1@arscenic.info>
2010-03-07 15:52:36 +0000

    Tentative pour faire fonctionner ce plugin sur la version 2.1 et 2.2 avec double définition dans le plugin.xml
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/forum@35906 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ spip21/forum_autoriser.php
@@ -0,0 +1,37 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2009                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+// declarer la fonction du pipeline
+function forum_autoriser(){}
+
+
+function autoriser_forum_interne_suivi_bouton_dist($faire, $type='', $id=0, $qui = NULL, $opt = NULL){
+	return true;
+}
+
+function autoriser_forum_reactions_bouton_dist($faire, $type='', $id=0, $qui = NULL, $opt = NULL){
+	return autoriser('publierdans','rubrique',_request('id_rubrique'));
+}
+
+function autoriser_bouton_forum_dist($faire, $type, $id, $qui, $opt){
+	return 	($GLOBALS['meta']['forum_prive'] != 'non' OR sql_countsel('spip_forum'));
+}
+
+function autoriser_bouton_forum_admin_dist($faire, $type, $id, $qui, $opt){
+	return 	($GLOBALS['meta']['forum_prive_admin'] == 'oui');
+}
+
+function autoriser_bouton_controle_forum_dist($faire, $type, $id, $qui, $opt){
+	return 	(sql_countsel('spip_forum'));
+}
+
+?>
