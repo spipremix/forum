93c225c3bc4f27984e0a1b1f4ef81aff8cc8283e
Matthieu Marcillaud <marcimat@rezo.net>
2010-03-09 15:47:34 +0000

    La gestion de la récupération des id_xxx dans l'environnement doit fonctionner même si on n'a pas ?var_mode=recalcul dans l'url :-) . On passe donc dans l'appel de la balise dynamique tous les ids potentiellement à récupérer, que l'on récupère réellement dans _stat. Et changement de nom du pipeline de liste d'objets potentiels en «forum_objets_depuis_env»
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/forum@35995 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- balise/formulaire_forum.php
+++ balise/formulaire_forum.php
@@ -26,62 +26,51 @@ include_spip('inc/forum');
 // dans le fichier d'appel, et si la table de reference est OK, proposer
 // la liste des mots-cles
 
+// #FORMULAIRE_FORUM seul calcule (objet, id_objet) depuis la boucle parente
+// #FORMULAIRE_FORUM{#SELF} pour forcer l'url de retour
+// #FORMULAIRE_FORUM{#SELF, article, 3} pour forcer l'objet et son identifiant
+
 // http://doc.spip.org/@balise_FORMULAIRE_FORUM
 function balise_FORMULAIRE_FORUM ($p) {
 	/**
-	 * Recuperation du type d'objet SPIP $objet
-	 * et de sa cle primaire $primary
-	 * depuis la boucle englobante
-	 * Code emprunté de crayons
-	 * Valable uniquement si l'on est dans une boucle
+	 * On recupere $objet et $id_objet depuis une boucle englobante si possible
+	 * Sinon, on essaie aussi de recuperer des id_xx dans l'URL qui pourraient indiquer
+	 * sur quoi le formulaire porte.
+	 * Enfin, on pourra aussi forcer objet et id_objet depuis l'appel du formulaire
 	 */
+
 	$i_boucle = $p->nom_boucle ? $p->nom_boucle : $p->id_boucle;
-	$table = $p->boucles[$i_boucle]->type_requete;
-	$ids = array();
-
-	if($table){
-		$objet = $table[strlen($table) - 1] == 's' ?
-			substr($table, 0, -1) :
-			str_replace(
-				array('syndication'),
-				array('site'),
-			$table);
-
-		$primary = $p->boucles[$i_boucle]->primary;
-	}
+	$_id_objet = $p->boucles[$i_boucle]->primary;
+	
 	/**
-	 * Sinon on essaye de trouver les forums en fonction de l'environnement
+	 * On essaye de trouver les forums en fonction de l'environnement
+	 * pour cela, on recupere l'ensemble des id_xxx possibles dans l'env
 	 */
-	else{
-		$objets = pipeline('forum_objet_accepte',array('id_article','id_rubrique','id_syndic','id_breve','id_ticket'));
-		foreach($objets as $objet){
-			if($val = _request($objet)){
-				$ids[$objet] = $val;
-			}
-		}
-		if(count($ids)>1){
-			if(isset($ids['id_rubrique'])){
-				unset($ids['id_rubrique']);
-			}
-		}
-
-		if(count($ids) == 1){
-			$primary = key($ids);
-			$objet = str_replace('id_','',$primary);
-			$table = table_objet($objet);
-		}else{
-			$primary = 'null';
-			$objet = 'null';
-			$table = 'null';
-		}
+	if (!$i_boucle) {
+		$ids = forum_get_objets_depuis_env();
+		$ids = array_values($ids);
 	}
 
-	$p = calculer_balise_dynamique($p,'FORMULAIRE_FORUM', array($primary,'id_forum', 'ajouter_mot', 'ajouter_groupe', 'afficher_texte'),array($objet,$table,$primary));
+	$obtenir = array(
+		'FORUM_TYPE_BOUCLE', // demande du type d'objet
+		$_id_objet,
+		'id_forum',
+		'ajouter_mot',
+		'ajouter_groupe',
+		'afficher_texte'
+	);
+	
+	if ($ids) {
+		$obtenir = array_merge($obtenir, $ids);
+	}
+
+	$p = calculer_balise_dynamique($p,'FORMULAIRE_FORUM', $obtenir);
 
 	// Ajouter le code d'invalideur specifique aux forums
 	include_spip('inc/invalideur');
-	if ($i = charger_fonction('code_invalideur_forums','',true))
+	if ($i = charger_fonction('code_invalideur_forums', '', true)) {
 		$p->code = $i($p, $p->code);
+	}
 
 	return $p;
 }
@@ -94,73 +83,191 @@ function balise_FORMULAIRE_FORUM ($p) {
 // http://doc.spip.org/@balise_FORMULAIRE_FORUM_stat
 function balise_FORMULAIRE_FORUM_stat($args, $context_compil) {
 
-	// le denier arg peut contenir l'url sur lequel faire le retour
+	// un arg peut contenir l'url sur lequel faire le retour
 	// exemple dans un squelette article.html : [(#FORMULAIRE_FORUM{#SELF})]
 	// recuperer les donnees du forum auquel on repond.
-	list ($ido, $idf, $am, $ag, $af, $url) = $args;
-	$ido = intval($ido);
-	$idf = intval($idf);
+	// deux autres a la suite pour forcer objet et id_objet
+	// [(#FORMULAIRE_FORUM{#SELF, article, 8})]
+	//
+	// $args = (obtenir) + (ids) + (url, objet, id_objet)
+	$nb_args_debut = 6;
+	list ($_objet, $ido, $idf, $am, $ag, $af) = $args;
 
-	$objet = $context_compil[5];
-	$table = $context_compil[6];
-	$primary = $context_compil[7];
+		$id_objet = intval(array_pop($args));
+		$objet    = array_pop($args);
+		$url      = array_pop($args);
+		$idf      = intval($idf);
 
-	if(!$primary){
-		if(intval($idf)){
-			$objet = sql_fetsel('id_objet,objet','spip_forum','id_forum='.intval($idf));
-			if(is_array($objet)){
-				$ido = $objet['id_objet'];
-				$objet = $objet['objet'];
-				$primary = id_table_objet($objet);
-				$table = table_objet($objet);
-			}else
-				return false;
-		}else
-			return false;
-	}
-
-	$type = substr($GLOBALS['meta']["forums_publics"],0,3);
-
-	if ($objet == 'article') {
-		$titre = sql_fetsel('accepter_forum AS type, titre', 'spip_articles', "statut = 'publie' AND id_article = $ido");
-		if ($titre) {
-			if ($titre['type']) $type = $titre['type'];
-		}
-		if ($type == 'non') return false;
+	// pas d'objet force ? on prend le type de boucle calcule
+	if (!$objet) {
+		$objet = $_objet;	
+		$id_objet = intval($ido); 
 	} else {
+		$id_objet = intval($id_objet); 
+	}
+	unset($_objet, $ido);
+	
+	// pas dans une boucle ? on generera une erreur ?
+	if ($objet == 'balise_hors_boucle') {
+		$objet = '';
+		$id_objet   = '';
+	} else {		
+		$objet = objet_type($objet);
+	}
 
-		if ($type == 'non') return false;
-
-		if ($objet == 'syndic') {
-			$titre = sql_fetsel('nom_site AS titre', 'spip_syndic', "statut = 'publie' AND id_syndic = $ido");
-		}else if($f = charger_fonction($objet.'_forum_extraire_titre','inc',true)){
-			$titre = $f($ido);
-		}else{
-			$table_objet_sql = table_objet_sql($objet);
-			$titre = sql_fetsel('titre', $table_objet_sql, "statut = 'publie' AND $primary = $ido");
+	// on tente de prendre l'objet issu de l'environnement si un n'a pas pu etre calcule
+	if (!$objet) {
+		$objets = forum_get_objets_depuis_env();
+		$ids = array(); $i = 0;
+		foreach ($objets as $o => $ido) {
+			if ($id = $args[$nb_args_debut + $i]) {
+				$ids[$o] = $id;
+			}
+			$i++;
+		}
+		spip_log($ids, 'forums');
+		if (count($ids)>1) {
+			if (isset($ids['rubrique'])) {
+				unset($ids['rubrique']);
+			}
+		}
+		if (count($ids) == 1) {
+			$objet = key($ids);
+			$id_objet = array_shift($ids);
 		}
 	}
+	unset ($i, $nb_args_debut);
 
-	if (!$titre) return false; // inexistant ou non public
-
-	if ($idf>0) {
-		$titre_m = sql_fetsel('titre', 'spip_forum', "id_forum = $idf");
-		if (!$titre_m) return false; // URL fabriquee
-		$titre = $titre_m;
+	
+	if (table_objet($objet) == 'syndic') {
+		$objet = 'site'; // eviter 'syndication' ... quelle *#@*&! de vilainerie 
 	}
 
-	if ($GLOBALS['meta']["mots_cles_forums"] != "oui")
-		$table = '';
+	// et si on n'a toujours pas ce qu'on souhaite, on tente de le trouver dans un forum existant...
+	if (!$id_objet and $idf){
+		if ($objet = sql_fetsel(array('id_objet', 'objet'), 'spip_forum', 'id_forum=' . sql_quote($idf))) {
+			$id_objet = $objet['id_objet'];
+			$objet = $objet['objet'];
+		} else {
+			return false;
+		}
+	}
+	// vraiment la... faut pas exagerer !
+	if (!$id_objet) {
+		return false;
+	}
 
-	$titre = supprimer_numero($titre['titre']);
+	// ca s'apparenterait presque a une autorisation...
+	// si on n'avait pas a envoyer la valeur $accepter_forum au formulaire
+	$accepter_forum = substr($GLOBALS['meta']["forums_publics"], 0, 3);
+	// il y a un cas particulier pour l'acceptation de forum d'article...
+	if ($f = charger_fonction($objet . '_accepter_forums_public', 'inc', true)){
+		$accepter_forum = $f($id_objet);
+	}
+	
+	if ($accepter_forum == 'non') {
+		return false;
+	}
+
+	if ($titre = forum_recuperer_titre($objet, $id_objet)) {
+		$titre = supprimer_numero($titre);
+	} else {
+		return false; // inexistant ou non public
+	}
+	
+	
+	if ($idf > 0) {
+		$titre_m = sql_fetsel('titre', 'spip_forum', "id_forum = " . sql_quote($idf));
+		if (!$titre_m) {
+			return false; // URL fabriquee
+		}
+		$titre = $titre_m['titre'];
+	}
+
+	$primary = id_table_objet($objet);
+	$table = table_objet($objet);
+	
+	if ($GLOBALS['meta']["mots_cles_forums"] != "oui") {
+		$table = '';
+	}
+
+	
 
 	// Sur quelle adresse va-t-on "boucler" pour la previsualisation ?
 	// si vide : self()
 	$script = '';
 
 	return
-		array($titre, $table, $type, $objet, $primary, $script,
-		$ido, $idf, $am, $ag, $af, $url);
+		array($titre, $table, $accepter_forum, $objet, $primary, $script,
+		$id_objet, $idf, $am, $ag, $af, $url);
+}
+
+
+// balise type_boucle de Rastapopoulos dans le plugin etiquettes
+// present aussi dans plugin ajaxforms, notations, et bien d'autres...
+function balise_FORUM_TYPE_BOUCLE($p) {
+	$type = $p->boucles[$p->id_boucle]->id_table;
+	$p->code = $type ? "'$type'" : "'balise_hors_boucle'";
+	return $p;  
+}
+
+
+function forum_recuperer_titre($objet, $id_objet) {
+	$titre = "";
+	
+	if ($f = charger_fonction($objet.'_forum_extraire_titre', 'inc', true)){
+		$titre = $f($id_objet);
+	} else {
+		// retrouver le titre grace a table_titre...
+		$trouver_table = charger_fonction('trouver_table', 'base');
+		$desc = $trouver_table(table_objet_sql($objet));
+		$_titre = $desc['titre'] ? $desc['titre'] : ($desc['field']['titre'] ? 'titre' : '');
+		$_table = $desc['table'];
+		$_primary = id_table_objet($_table);
+		if ($_titre and $res = sql_fetsel($_titre, $_table, array(
+			"statut = " . sql_quote('publie'),
+			"$_primary = ". sql_quote($id_objet))
+		)) {
+			$titre = $res['titre'];
+		}
+	}
+	
+	return $titre;
+}
+
+
+
+function inc_article_accepter_forums_public_dist($id_objet) {
+	$accepter_forum = substr($GLOBALS['meta']["forums_publics"],0,3);
+	$art_accepter_forum = sql_getfetsel('accepter_forum', 'spip_articles', array(
+		"statut = ". sql_quote('publie'),
+		"id_article = ". sql_quote($id)
+	));
+	if ($art_accepter_forum) {
+		$accepter_forum = $art_accepter_forum;
+	}
+	return $accepter_forum;
+}
+
+
+// recuperer tous les objets dont on veut pouvoir obtenir l'identifiant
+// directement dans l'environnement
+function forum_get_objets_depuis_env() {
+	static $objets = null;
+	if ($objets === null) {
+		// on met une cle (le type d'objet) pour qu'un appel du pipeline
+		// puisse facilement soustraire un objet qu'il ne veut pas avec
+		// unset($objets['rubrique']) par exemple.
+		$objets = pipeline('forum_objets_depuis_env', array(
+			'article' => id_table_objet('article'),
+			'rubrique' => id_table_objet('rubrique'),
+			'site' => id_table_objet('site'),
+			'breve' => id_table_objet('breve')
+		));
+		asort($objets);
+	}
+
+	return $objets;
 }
 
 ?>
