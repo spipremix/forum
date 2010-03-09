bb6f5785521f895f8a79a3783ae3a460193a5946
Matthieu Marcillaud <marcimat@rezo.net>
2010-03-09 15:47:34 +0000

    La gestion de la récupération des id_xxx dans l'environnement doit fonctionner même si on n'a pas ?var_mode=recalcul dans l'url :-) . On passe donc dans l'appel de la balise dynamique tous les ids potentiellement à récupérer, que l'on récupère réellement dans _stat. Et changement de nom du pipeline de liste d'objets potentiels en «forum_objets_depuis_env»

--- forum_options.php
+++ forum_options.php
@@ -1,10 +1,12 @@
 <?php
 
 /**
- * Déclaration du pipeline forum_objet_accepte
- * Correspond à un array des clés primaires des tables qui acceptent un forum
+ * Declaration du pipeline forum_objets_depuis_url
+ * Correspond a un array objet/cles primaires des tables qui acceptent
+ * qu'un forum recherche dans l'url appelee d'identifiant sur lequel s'appliquera
+ * le forum, de sorte que son appel ne se trouve pas necessairement
+ * dans une boucle englobante
  */
-
-$GLOBALS['spip_pipeline']['forum_objet_accepte'] = '';
+$GLOBALS['spip_pipeline']['forum_objets_depuis_env'] = '';
 
 ?>
