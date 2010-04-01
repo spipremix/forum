ba864150af74364d8cfce5bd799a6d6936e779a1
klaus <klaus@spip.de>
2010-04-01 20:47:24 +0000

    traduction non testée, merci de me le signaler s'il y a des fautes techniques
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/forum@36833 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lang/forum_de.php
@@ -0,0 +1,50 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'aucun_message_forum' => 'Kein Beitrag',
+'un_message_forum' => '1 Beitrag im Forum',
+'nombre_messages_forum' => '@nb@ Beitr&auml;ge im Forum',
+
+'icone_bruler_message' => "Als SPAM markieren",
+'icone_valider_repondre_message' => 'Beantworten dieses Beitrags best&auml;tigen',
+
+'icone_valider_messages' => 'Diese Beitr&auml;ge freischalten',
+'icone_bruler_messages' => "Als SPAM markieren",
+'icone_legitimer_message' => "Als erw&uuml;nscht markieren",
+'icone_supprimer_messages' => 'Diese Beitr&auml;ge l&ouml;schen',
+'interface_onglets' => 'Interface mit Reitern',
+'interface_formulaire' => 'Interface als Formular',
+
+'messages_meme_auteur' => 'Alle Beitr&auml;ge dieses Autors',
+'messages_meme_email' => 'Alle Beitr&auml;ge mit dieser E-Mail',
+'messages_meme_ip' => 'Alle Beitr&auml;ge von dieser IP',
+'texte_en_cours_validation'=> 'Diese Artikel, Meldungen und Forumsbeitr&auml;ge sind zur Ver&ouml;ffentlichung vorgeschlagen',
+
+// statuts
+'messages_tous' => 'Alle',
+'messages_aucun' => 'Kein',
+'messages_publie' => 'Ver&ouml;ffentlicht',
+'messages_prop' => 'Vorgeschlagen',
+'messages_spam' => 'Spam',
+'messages_off' => 'Gel&ouml;scht',
+
+'messages_prive' => 'Privat',
+'messages_privoff' => 'Gel&ouml;scht',
+'messages_privrac' => 'Allgemein',
+'messages_privadm' => 'Administratoren',
+
+'messages_perso' => 'Pers&ouml;nlich',
+
+'statut_prop' => 'Vorgeschlagen',
+'statut_publie' => 'Ver&ouml;ffentlicht',
+'statut_spam' => 'Spam',
+'statut_off' => 'Gel&ouml;scht',
+
+'tout_voir' => 'Alle Beitr&auml;ge anzeigen',
+);
+
+
+?>
