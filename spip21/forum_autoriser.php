6716c52c09bf952a44c14c54bfe11c6bb937922e
Fil <fil@rezo.net>
2010-08-20 21:53:39 +0000

    report de [15960] [15961] sur les autorisations

--- spip21/forum_autoriser.php
+++ spip21/forum_autoriser.php
@@ -42,4 +42,16 @@ if(!function_exists('autoriser_bouton_controle_forum_dist')){
 		return 	(sql_countsel('spip_forum'));
 	}
 }
+
+// Consulter le forum des admins ?
+// admins y compris restreints
+// http://doc.spip.org/@autoriser_forum_admin_dist
+if(!function_exists('autoriser_forum_admin_dist')){
+function autoriser_forum_admin_dist($faire, $type, $id, $qui, $opt) {
+	return
+		$qui['statut'] == '0minirezo'
+		;
+}
+}
+
 ?>
