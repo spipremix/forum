dc7c520505f3d8dbe4bb0b14cce7eb8360297dd3
Cerdic <cedric@yterium.com>
2011-03-14 13:18:37 +0000

    suite a http://core.spip.org/projects/spip/repository/revisions/17398 on conserve generer_url_ecrire_forum qui gere le cas particulier debut_forum=@xxx  mais on change sa signature, le flag 'statut' devenant 'plublic'

--- urls/generer_url_ecrire_forum.php
+++ urls/generer_url_ecrire_forum.php
@@ -11,12 +11,11 @@
 \***************************************************************************/
 
 // http://doc.spip.org/@generer_url_ecrire_forum
-function urls_generer_url_ecrire_forum_dist($id, $args='', $ancre='', $statut='', $connect='') {
+function urls_generer_url_ecrire_forum_dist($id, $args='', $ancre='', $public=null, $connect='') {
 	$a = "id_forum=" . intval($id);
-	if (!$statut) {
-		$statut = sql_getfetsel('statut', 'spip_forum', $a,'','','','',$connect);
-	}
-	$h = ($statut == 'publie' OR $connect)
+	if (is_null($public) AND !$connect)
+		$public = objet_test_si_publie($objet, $id, $connect);
+	$h = ($public OR $connect)
 	?  generer_url_entite_absolue($id, 'forum', $args, $ancre, $connect)
 	: (generer_url_ecrire('controler_forum', "debut_forum=@$id" . ($args ? "&$args" : ''))
 		. ($ancre ? "#$ancre" : ''));
