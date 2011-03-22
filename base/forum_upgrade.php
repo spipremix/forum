b53ad78a7279a0c234e7bb4f523b655216e0e712
Cerdic <cedric@yterium.com>
2011-03-22 14:48:15 +0000

    exemple d'utilisation de la fonction maj_plugin par descriptions des upgrades unitaires dans un tableau rangé par numéro de version. introduit par http://core.spip.org/projects/spip/repository/revisions/17472
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/forum@45724 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- base/forum_upgrade.php
+++ base/forum_upgrade.php
@@ -22,6 +22,7 @@ function forum_upgrade($nom_meta_base_version,$version_cible){
 	if ( (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
 			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
 
+		// installation des tables
 		if (version_compare($current_version, '0.0','<=')){
 			include_spip('base/forum');
 			include_spip('base/create');
@@ -33,36 +34,34 @@ function forum_upgrade($nom_meta_base_version,$version_cible){
 			ecrire_meta($nom_meta_base_version,$current_version = '1.0');
 		}
 
-		# mise a jour de (id_article,id_breve,...) vers (objet,id_objet)
-		if (version_compare($current_version, '1.1','<')) {
-			echo "<h4>MISE A JOUR DES FORUMS (objet,id_objet)</h4>";
-			sql_alter("TABLE `spip_forum` ADD `id_objet` bigint(21) DEFAULT 0 NOT NULL AFTER `id_forum`");
-			sql_alter("TABLE `spip_forum` ADD `objet` VARCHAR (25) DEFAULT '' NOT NULL AFTER `id_objet`");
-			sql_alter("TABLE `spip_forum` DROP key `optimal`");
-			sql_alter("TABLE `spip_forum` ADD key `optimal` (`statut`,`id_parent`,`id_objet`,`objet`,`date_heure`)");
-
-			echo "<ul>";
-
-			foreach(array('breve', 'article', 'syndic', 'message', 'rubrique')
-			as $objet) {
-				echo "<li>$objet</li>";
-				sql_update("spip_forum", array(
-					'objet' => sql_quote($objet),
-					'id_objet' => 'id_'.$objet
-				), 'id_'.$objet.' > 0');
-				sql_alter('TABLE `spip_forum` DROP `id_'.$objet.'`');
-			}
-
-			echo "</ul>";
-
-			ecrire_meta($nom_meta_base_version,$current_version = '1.1');
-		}
+		$maj = array();
+		$maj['1.1.0'] = array(
+			array('sql_alter',"TABLE spip_forum ADD id_objet bigint(21) DEFAULT 0 NOT NULL AFTER id_forum"),
+			array('sql_alter',"TABLE spip_forum ADD objet VARCHAR (25) DEFAULT '' NOT NULL AFTER id_objet"),
+			array('sql_alter',"TABLE spip_forum DROP key optimal"),
+			array('sql_alter',"TABLE spip_forum ADD key optimal (statut,id_parent,id_objet,objet,date_heure)"),
+		);
+		$maj['1.1.1'] = array(
+			array('sql_update',"spip_forum", array('objet' => 'breve','id_objet' => 'id_breve'), 'id_breve> 0'),
+			array('sql_alter',"TABLE spip_forum DROP id_breve"),
+			array('sql_update',"spip_forum", array('objet' => 'article','id_objet' => 'id_article'), 'id_article>0'),
+			array('sql_alter',"TABLE spip_forum DROP id_article"),
+			array('sql_update',"spip_forum", array('objet' => 'site','id_objet' => 'id_syndic'), 'id_syndic>0'),
+			array('sql_alter',"TABLE spip_forum DROP id_syndic"),
+			array('sql_update',"spip_forum", array('objet' => 'message','id_objet' => 'id_message'), 'id_message>0'),
+			array('sql_alter',"TABLE spip_forum DROP id_message"),
+			array('sql_update',"spip_forum", array('objet' => 'rubrique','id_objet' => 'id_rubrique'), 'id_rubrique>0'),
+			array('sql_alter',"TABLE spip_forum DROP id_rubrique"),
+		);
 
 		# champ ip sur 40 car (compat IPv6)
-		if (version_compare($current_version, '1.2','<')) {
-			sql_alter("TABLE `spip_forum` CHANGE `ip` `ip` VRCHAR(40) DEFAULT '' NOT NULL");
-			ecrire_meta($nom_meta_base_version,$current_version = '1.2');
-		}
+		$maj['1.2.0'] = array(
+			array('sql_alter',"TABLE spip_forum CHANGE ip ip VARCHAR(40) DEFAULT '' NOT NULL"),
+		);
+
+		include_spip('base/upgrade');
+		maj_plugin($nom_meta_base_version, $version_cible, $maj);
+
 	}
 }
 
