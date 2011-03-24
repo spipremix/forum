44896565b5e28e5fd62730275d88f9700817c20b
Cerdic <cedric@yterium.com>
2011-03-24 11:30:38 +0000

    utilisation de l'index 'create' de $maj, comme proposé par http://core.spip.org/projects/spip/repository/revisions/17487 Il reste ici simplement le cas de l'exception a gérer lorsque la base existe déjà (migration depuis SPIP <=2.1)
    On voit que la fonction d'upgrade se simplifie, et pour achever cela il suffirait de deléguer a la fonction appelante le lancement de maj_plugin() par un simple 
    return $maj;

--- base/forum_upgrade.php
+++ base/forum_upgrade.php
@@ -17,52 +17,50 @@
  * @param string $version_cible
  */
 function forum_upgrade($nom_meta_base_version,$version_cible){
-	$current_version = '0.0';
 
-	if ( (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
-			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
-
-		// installation des tables
-		if (version_compare($current_version, '0.0','<=')){
-			include_spip('base/forum');
-			include_spip('base/create');
-			// creer les tables
-			creer_base();
-			// mettre les metas par defaut
-			$config = charger_fonction('config','inc');
-			$config();
-			ecrire_meta($nom_meta_base_version,$current_version = '1.0');
+	// cas particulier :
+	// si plugin pas installe mais que la table existe
+	// considerer que c'est un upgrade depuis v 1.0.0
+	// pour gerer l'historique des installations SPIP <=2.1
+	if (!isset($GLOBALS['meta'][$nom_meta_base_version])){
+		$trouver_table = charger_fonction('trouver_table','base');
+		if ($desc = $trouver_table('spip_forum')
+		  AND isset($desc['field'])){
+			ecrire_meta($nom_meta_base_version,'1.0.0');
 		}
-
-		$maj = array();
-		$maj['1.1.0'] = array(
-			array('sql_alter',"TABLE spip_forum ADD id_objet bigint(21) DEFAULT 0 NOT NULL AFTER id_forum"),
-			array('sql_alter',"TABLE spip_forum ADD objet VARCHAR (25) DEFAULT '' NOT NULL AFTER id_objet"),
-			array('sql_alter',"TABLE spip_forum DROP key optimal"),
-			array('sql_alter',"TABLE spip_forum ADD key optimal (statut,id_parent,id_objet,objet,date_heure)"),
-		);
-		$maj['1.1.1'] = array(
-			array('sql_update',"spip_forum", array('objet' => 'breve','id_objet' => 'id_breve'), 'id_breve> 0'),
-			array('sql_alter',"TABLE spip_forum DROP id_breve"),
-			array('sql_update',"spip_forum", array('objet' => 'article','id_objet' => 'id_article'), 'id_article>0'),
-			array('sql_alter',"TABLE spip_forum DROP id_article"),
-			array('sql_update',"spip_forum", array('objet' => 'site','id_objet' => 'id_syndic'), 'id_syndic>0'),
-			array('sql_alter',"TABLE spip_forum DROP id_syndic"),
-			array('sql_update',"spip_forum", array('objet' => 'message','id_objet' => 'id_message'), 'id_message>0'),
-			array('sql_alter',"TABLE spip_forum DROP id_message"),
-			array('sql_update',"spip_forum", array('objet' => 'rubrique','id_objet' => 'id_rubrique'), 'id_rubrique>0'),
-			array('sql_alter',"TABLE spip_forum DROP id_rubrique"),
-		);
-
-		# champ ip sur 40 car (compat IPv6)
-		$maj['1.2.0'] = array(
-			array('sql_alter',"TABLE spip_forum CHANGE ip ip VARCHAR(40) DEFAULT '' NOT NULL"),
-		);
-
-		include_spip('base/upgrade');
-		maj_plugin($nom_meta_base_version, $version_cible, $maj);
-
+		// si pas de table en base, on fera une simple creation de base
 	}
+
+	$maj = array();
+	$maj['create'] = array(
+		array('maj_tables',array('spip_forum')),
+	);
+	$maj['1.1.0'] = array(
+		array('sql_alter',"TABLE spip_forum ADD id_objet bigint(21) DEFAULT 0 NOT NULL AFTER id_forum"),
+		array('sql_alter',"TABLE spip_forum ADD objet VARCHAR (25) DEFAULT '' NOT NULL AFTER id_objet"),
+		array('sql_alter',"TABLE spip_forum DROP key optimal"),
+		array('sql_alter',"TABLE spip_forum ADD key optimal (statut,id_parent,id_objet,objet,date_heure)"),
+	);
+	$maj['1.1.1'] = array(
+		array('sql_update',"spip_forum", array('objet' => 'breve','id_objet' => 'id_breve'), 'id_breve> 0'),
+		array('sql_alter',"TABLE spip_forum DROP id_breve"),
+		array('sql_update',"spip_forum", array('objet' => 'article','id_objet' => 'id_article'), 'id_article>0'),
+		array('sql_alter',"TABLE spip_forum DROP id_article"),
+		array('sql_update',"spip_forum", array('objet' => 'site','id_objet' => 'id_syndic'), 'id_syndic>0'),
+		array('sql_alter',"TABLE spip_forum DROP id_syndic"),
+		array('sql_update',"spip_forum", array('objet' => 'message','id_objet' => 'id_message'), 'id_message>0'),
+		array('sql_alter',"TABLE spip_forum DROP id_message"),
+		array('sql_update',"spip_forum", array('objet' => 'rubrique','id_objet' => 'id_rubrique'), 'id_rubrique>0'),
+		array('sql_alter',"TABLE spip_forum DROP id_rubrique"),
+	);
+
+	# champ ip sur 40 car (compat IPv6)
+	$maj['1.2.0'] = array(
+		array('sql_alter',"TABLE spip_forum CHANGE ip ip VARCHAR(40) DEFAULT '' NOT NULL"),
+	);
+
+	include_spip('base/upgrade');
+	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
 /**
