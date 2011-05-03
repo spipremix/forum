2967ea10932c39b43cdb450cafdd8de304e9cfd8
Cerdic <cedric@yterium.com>
2011-05-03 08:25:57 +0000

    clause SQL DROP KEY erronée qui faisait échouer l'upgrade sous SQLITE. On corrige en DROP INDEX, et on rejoue cette partie de migration.

--- base/forum_upgrade.php
+++ base/forum_upgrade.php
@@ -38,25 +38,37 @@ function forum_upgrade($nom_meta_base_version,$version_cible){
 	$maj['1.1.0'] = array(
 		array('sql_alter',"TABLE spip_forum ADD id_objet bigint(21) DEFAULT 0 NOT NULL AFTER id_forum"),
 		array('sql_alter',"TABLE spip_forum ADD objet VARCHAR (25) DEFAULT '' NOT NULL AFTER id_objet"),
-		array('sql_alter',"TABLE spip_forum DROP key optimal"),
-		array('sql_alter',"TABLE spip_forum ADD key optimal (statut,id_parent,id_objet,objet,date_heure)"),
+		#array('sql_alter',"TABLE spip_forum DROP INDEX optimal"),
+		#array('sql_alter',"TABLE spip_forum ADD INDEX optimal (statut,id_parent,id_objet,objet,date_heure)"),
 	);
 	$maj['1.1.1'] = array(
 		array('sql_update',"spip_forum", array('objet' => 'breve','id_objet' => 'id_breve'), 'id_breve> 0'),
-		array('sql_alter',"TABLE spip_forum DROP id_breve"),
+		#array('sql_alter',"TABLE spip_forum DROP id_breve"),
 		array('sql_update',"spip_forum", array('objet' => 'article','id_objet' => 'id_article'), 'id_article>0'),
-		array('sql_alter',"TABLE spip_forum DROP id_article"),
+		#array('sql_alter',"TABLE spip_forum DROP id_article"),
 		array('sql_update',"spip_forum", array('objet' => 'site','id_objet' => 'id_syndic'), 'id_syndic>0'),
-		array('sql_alter',"TABLE spip_forum DROP id_syndic"),
+		#array('sql_alter',"TABLE spip_forum DROP id_syndic"),
 		array('sql_update',"spip_forum", array('objet' => 'message','id_objet' => 'id_message'), 'id_message>0'),
-		array('sql_alter',"TABLE spip_forum DROP id_message"),
+		#array('sql_alter',"TABLE spip_forum DROP id_message"),
 		array('sql_update',"spip_forum", array('objet' => 'rubrique','id_objet' => 'id_rubrique'), 'id_rubrique>0'),
-		array('sql_alter',"TABLE spip_forum DROP id_rubrique"),
+		#array('sql_alter',"TABLE spip_forum DROP id_rubrique"),
 	);
 
 	# champ ip sur 40 car (compat IPv6)
 	$maj['1.2.0'] = array(
 		array('sql_alter',"TABLE spip_forum CHANGE ip ip VARCHAR(40) DEFAULT '' NOT NULL"),
+	);
+	# rejouer la suppression/creation de l'index optimal
+	# et la suppression des vieux champs, car la premiere sequence avait echoue
+	# en raison d'un DROP KEY au lieu de DROP INDEX
+	$maj['1.2.1'] = array(
+		array('sql_alter',"TABLE spip_forum DROP INDEX optimal"),
+		array('sql_alter',"TABLE spip_forum ADD INDEX optimal (statut,id_parent,id_objet,objet,date_heure)"),
+		array('sql_alter',"TABLE spip_forum DROP id_breve"),
+		array('sql_alter',"TABLE spip_forum DROP id_article"),
+		array('sql_alter',"TABLE spip_forum DROP id_syndic"),
+		array('sql_alter',"TABLE spip_forum DROP id_message"),
+		array('sql_alter',"TABLE spip_forum DROP id_rubrique"),
 	);
 
 	include_spip('base/upgrade');
