1bd804ec886483b84365019027cf4c54032a1fa1
jluc <jluc@no-log.org>
2011-11-20 09:33:22 +0000

    présentation des pages de configuration ( ticket http://core.spip.org/issues/2408  )

--- lang/forum_fr.php
+++ lang/forum_fr.php
@@ -79,8 +79,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_valider_messages' => 'Valider ces messages',
 	'icone_valider_repondre_message' => 'Valider &amp; Répondre à ce message',
 	'info_1_message_forum' => '1 message de forum',
-	'info_activer_forum_public' => '<i>Pour activer les forums publics, veuillez choisir leur mode
-	de modération par défaut:</i>',
+	'info_activer_forum_public' => 'Pour activer les forums publics, veuillez choisir leur mode
+	de modération par défaut&nbsp;:',
 	'info_appliquer_choix_moderation' => 'Appliquer ce choix de modération :',
 	'info_config_forums_prive' => 'Dans l’espace privé du site, vous pouvez activer plusieurs types de forums :',
 	'info_config_forums_prive_admin' => 'Un forum réservé aux administrateurs du site :',
@@ -104,8 +104,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_nb_messages_forum' => '@nb@ messages de forum',
 	'info_option_email' => 'Lorsqu\'un visiteur du site poste un nouveau message dans le forum associé à un article, les auteurs de l\'article peuvent être prévenus de ce message par e-mail. Indiquer pour chaque type de forum s\'il faut utiliser cette option.',
 	'info_pas_de_forum' => 'pas de forum',
-	'info_question_visiteur_ajout_document_forum' => 'Si vous souhaitez autoriser les visiteurs à joindre des documents (images, sons...) à leurs messages de forum, indiquer ci-dessous la liste des extensions de documents autorisés pour les forums (ex: gif, jpg, png, mp3).',
-	'info_question_visiteur_ajout_document_forum_format' => 'Si vous souhaitez autoriser tous les types de documents considérés comme fiables par SPIP, mettre une étoile. Pour ne rien autoriser, ne rien indiquer.',
+	'info_question_visiteur_ajout_document_forum' => 'Si vous souhaitez autoriser les visiteurs à joindre des documents (images, sons...) à leurs messages de forum, indiquez ci-dessous la liste des extensions de documents autorisés pour les forums (ex: gif, jpg, png, mp3).',
+	'info_question_visiteur_ajout_document_forum_format' => 'Si vous souhaitez autoriser tous les types de documents considérés comme fiables par SPIP, mettez une étoile. Pour ne rien autoriser, n\'indiquez rien.',
 	'interface_formulaire' => 'Interface formulaire',
 	'interface_onglets' => 'Interface avec onglets',
 	'item_activer_forum_administrateur' => 'Activer le forum des administrateurs',
