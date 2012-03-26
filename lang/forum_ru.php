1a0c4109aa24d5d10363d74cf66821c147089ae3
davoodhossein <davoodhossein@yahoo.com>
2012-03-26 08:45:28 +0000

    langues (forum)
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/forum@59726 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/forum_ru.php
+++ lang/forum_ru.php
@@ -11,21 +11,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'aucun_message_forum' => 'Aucun message de forum', # NEW
 
 	// B
-	'bouton_radio_articles_futurs' => 'только для новых статей(текущие настройки не изменяться).',
-	'bouton_radio_articles_tous' => 'ко всем статьям без исключения.',
-	'bouton_radio_articles_tous_sauf_forum_desactive' => 'ко всем статьям, за исключением закрытых  форумов.',
-	'bouton_radio_enregistrement_obligatoire' => 'Требуется регистрация (
- пользователи должны подписаться, предоставляя свой электронный адрес, перед 
-  возможностью публикации статьи).',
-	'bouton_radio_moderation_priori' => 'Пре-модерация(
- статьи будут показываться только после утверждения 
- администраторами).', # MODIF
-	'bouton_radio_modere_abonnement' => 'запрос на регистрацию',
+	'bouton_radio_articles_futurs' => 'только для новых материалов (текущие настройки не изменятся).',
+	'bouton_radio_articles_tous' => 'для всех статей без исключения.',
+	'bouton_radio_articles_tous_sauf_forum_desactive' => 'для всех статей, кроме тех, в которых комментирование отключено.',
+	'bouton_radio_enregistrement_obligatoire' => 'Только для зарегистрированных пользователей. Посетитель должен зарегистрироваться перед тем как оставить комментарий.',
+	'bouton_radio_moderation_priori' => 'Комментарии публикуются на сайте после проверки администратором (пре-модерация).',
+	'bouton_radio_modere_abonnement' => 'необходимо зарегистрироваться',
 	'bouton_radio_modere_posteriori' => 'пост-модерация', # MODIF
 	'bouton_radio_modere_priori' => 'пре-модерация', # MODIF
-	'bouton_radio_publication_immediate' => 'Немедленная публикация сообщений 
-(статьи будут показываться, как только они будут отправлены. Администраторы могут 
- потом удалить их).',
+	'bouton_radio_publication_immediate' => 'Новые комментарии сразу публикуются на сайте без проверки (пост-модерация). Администраторы потом могут удалить комментарии.',
 
 	// D
 	'documents_interdits_forum' => 'Запрещенно добавлять документы к сообщениям форума',
@@ -45,21 +39,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'forum_cliquer_retour' => 'Нажмите  <a href=\'@retour_forum@\'>сюда</a> для продолжения.',
 	'forum_forum' => 'форум',
 	'forum_info_modere' => 'Этот форум модерируется заранее: ваше сообщение появиться после того как его утвердит администратор.', # MODIF
-	'forum_lien_hyper' => '<b>Ссылка</b> (необязательно)', # MODIF
+	'forum_lien_hyper' => 'Ссылка',
 	'forum_message' => 'Votre message', # NEW
 	'forum_message_definitif' => 'Всё верно! Отправить!',
 	'forum_message_trop_long' => 'Ваше сообщение слишком длинно. Максимальная длина 20000 символов.', # MODIF
-	'forum_ne_repondez_pas' => 'Не отвечайте на эту почту  на форуме в следующих адресеах:',
+	'forum_ne_repondez_pas' => 'Не отвечайте на это письмо - перейдите по ссылке:',
 	'forum_page_url' => '(Если ваше сообщение обращено к статьям изданным в сети или к странице, обеспечивающей дальнейшую информацию, пожалуйста введите заголовок страницы и ее URL ниже).',
 	'forum_permalink' => 'Lien permanent vers le commentaire', # NEW
 	'forum_poste_par' => '@parauteur@ прокомментировал вашу статью.',
-	'forum_qui_etes_vous' => '<b>Кто вы?</b> (необязательно)', # MODIF
+	'forum_qui_etes_vous' => 'Кто вы?',
 	'forum_saisie_texte_info' => 'Ce formulaire accepte les raccourcis SPIP <code>[-&gt;url] {{gras}} {italique} &lt;quote&gt; &lt;code&gt;</code> et le code HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Pour créer des paragraphes, laissez simplement des lignes vides.', # NEW
-	'forum_texte' => 'Текст сообщения:', # MODIF
+	'forum_texte' => 'Комментарий:',
 	'forum_titre' => 'Тема:', # MODIF
 	'forum_url' => 'URL:', # MODIF
 	'forum_valider' => 'Подтвердите выбор',
-	'forum_voir_avant' => 'Предварительный просмотр сообщения перед отправлением', # MODIF
+	'forum_voir_avant' => 'Предварительный просмотр',
 	'forum_votre_email' => 'Ваш e-mail адрес:', # MODIF
 	'forum_votre_nom' => 'Ваше имя (или псевдоним):', # MODIF
 	'forum_vous_enregistrer' => 'Для участия в
@@ -80,15 +74,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_valider_messages' => 'Valider ces messages', # NEW
 	'icone_valider_repondre_message' => 'Valider & Répondre à ce message', # NEW
 	'info_1_message_forum' => '1 message de forum', # NEW
-	'info_activer_forum_public' => '<i>Для включения публичных форумов, пожалуйста выберите модерацию: </i>', # MODIF
+	'info_activer_forum_public' => 'Включить комментарии - выберите режим модерации для новых сообщений:',
 	'info_appliquer_choix_moderation' => 'Применить этот тип модерации:',
 	'info_config_forums_prive' => 'В области редактирования сайта может быть включено несколько видов форума:', # MODIF
 	'info_config_forums_prive_admin' => 'Форум для администраторов сайта:',
 	'info_config_forums_prive_global' => 'Общий форум, открыт для всех авторов:',
 	'info_config_forums_prive_objets' => 'Форум, прикрепленный к каждой статье, новости, ссылочному сайту, и т. д.:',
-	'info_desactiver_forum_public' => 'Отключить основные 
- форумы. Основные форумы могут быть разрешены в зависимости от конкретного случая 
-  основы статей; они будут запрещены для разделов, новостей, и т.д.', # MODIF
+	'info_desactiver_forum_public' => 'Отключить комментарии по сайту. Вы можете отдельно разрешить комментировать конкретные статьи, но в целом комментарии на сайте отключены.',
 	'info_envoi_forum' => 'Отправлять сообщения форумов авторам статей',
 	'info_fonctionnement_forum' => 'Работа форума:',
 	'info_forums_liees_mot' => 'Les messages de forum liés à ce mot', # NEW
@@ -100,7 +92,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_liens_syndiques_7' => 'ожидание утверждения.',
 	'info_liens_texte' => 'Lien(s) contenu(s) dans le texte du message', # NEW
 	'info_liens_titre' => 'Lien(s) contenu(s) dans le titre du message', # NEW
-	'info_mode_fonctionnement_defaut_forum_public' => 'Стандартный режим работы основных форумов',
+	'info_mode_fonctionnement_defaut_forum_public' => 'Настройки комментариев на сайте',
 	'info_nb_messages_forum' => '@nb@ messages de forum', # NEW
 	'info_option_email' => 'Когда посетитель сайта размещает сообщение на 
  форуме  связанного со статьей, автор статьи может быть 
@@ -138,7 +130,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'messages_prop' => 'Proposés', # NEW
 	'messages_publie' => 'Publiés', # NEW
 	'messages_spam' => 'Spam', # NEW
-	'messages_tous' => 'Tous', # NEW
+	'messages_tous' => 'Все',
 
 	// O
 	'onglet_messages_internes' => 'Внутреняя переписка',
@@ -153,7 +145,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'statut_original' => 'Исходник',
 	'statut_prop' => 'Proposé', # NEW
 	'statut_publie' => 'Publié', # NEW
-	'statut_spam' => 'Spam', # NEW
+	'statut_spam' => 'Спам',
 
 	// T
 	'text_article_propose_publication_forum' => 'Вы можете прокомментировать эту статью, используя прикрепленный форум (внизу страницы).',
