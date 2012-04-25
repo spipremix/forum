5bd81dfd36848f56047af61e9a84fa82ec2332d2
elvis.deltedesco <elvis.deltedesco@phante.com>
2012-04-25 06:37:12 +0000

    langues (forum)

--- lang/forum_it.php
+++ lang/forum_it.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'aucun_message_forum' => 'Aucun message de forum', # NEW
+	'aucun_message_forum' => 'Nessun messaggio sul forum',
 
 	// B
 	'bouton_radio_articles_futurs' => 'solo agli articoli inseriti dopo questa modifica (nessuna modifica al database attuale).',
@@ -18,11 +18,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 utenti devono abbonarsi fornendo il loro indirizzo email prima di
 poter inviare contributi).',
 	'bouton_radio_moderation_priori' => 'Moderazione a priori (i
-contributi saranno pubblicati previa autorizzazione degli
-amministratori).', # MODIF
+contributi saranno pubblicati dopo validazione da parte degli
+amministratori).',
 	'bouton_radio_modere_abonnement' => 'solo su abbonamento',
-	'bouton_radio_modere_posteriori' => 'moderato a posteriori', # MODIF
-	'bouton_radio_modere_priori' => 'moderato a priori', # MODIF
+	'bouton_radio_modere_posteriori' => 'moderato a posteriori',
+	'bouton_radio_modere_priori' => 'moderato a priori',
 	'bouton_radio_publication_immediate' => 'Pubblicazione immediata dei messaggi
 (i contributi sono pubblicati subito dopo il loro invio, gli amministratori possono
 cancellarli successivamente).',
@@ -31,59 +31,58 @@ cancellarli successivamente).',
 	'documents_interdits_forum' => 'Documenti vietati nel forum',
 
 	// E
-	'erreur_enregistrement_message' => 'Votre message n\'a pas pu être enregistré en raison d\'un problème technique', # NEW
+	'erreur_enregistrement_message' => 'Il tuo messaggio non può essere salvato a causa di un problema tecnico.',
 
 	// F
 	'form_pet_message_commentaire' => 'Un messaggio, un commento?',
 	'forum' => 'Forum',
 	'forum_acces_refuse' => 'Non hai accesso a questi forum.',
 	'forum_attention_dix_caracteres' => '<b>Attenzione!</b> il messaggio deve contenere almeno dieci caratteri.',
-	'forum_attention_message_non_poste' => 'Attention, vous n\'avez pas posté votre message !', # NEW
+	'forum_attention_message_non_poste' => 'Attenzione, non puoi postare il tuo messaggio!',
 	'forum_attention_trois_caracteres' => '<b>Attenzione!</b> il titolo deve contenere almeno tre caratteri.',
-	'forum_attention_trop_caracteres' => '<b>Attenzione!</b> il messaggio è troppo lungo (@compte@ caratteri): per poter essere registrato esso non deve essere più lungo di @max@ caratteri.', # MODIF
+	'forum_attention_trop_caracteres' => '<b>Attenzione!</b> il messaggio è troppo lungo (@compte@ caratteri): per poter essere registrato esso non deve essere più lungo di @max@ caratteri.',
 	'forum_avez_selectionne' => 'Hai selezionato:',
 	'forum_cliquer_retour' => 'Clicca <a href=\'@retour_forum@\'>qui</a> per continuare.',
 	'forum_forum' => 'forum',
-	'forum_info_modere' => 'Questo forum è moderato a priori: il tuo contributo apparirà solo dopo essere stato approvato da un amministratore del sito.', # MODIF
-	'forum_lien_hyper' => '<b>Link ipertestuale</b> (opzionale)', # MODIF
-	'forum_message' => 'Votre message', # NEW
+	'forum_info_modere' => 'Questo forum è moderato a priori: il tuo contributo apparirà solo dopo essere stato approvato da un amministratore del sito.',
+	'forum_lien_hyper' => 'Link ipertestuale',
+	'forum_message' => 'I tuoi messaggi',
 	'forum_message_definitif' => 'Messaggio definitivo: invialo al sito',
-	'forum_message_trop_long' => 'Il tuo messaggio è troppo lungo. La dimensione massima è di 20000 caratteri.', # MODIF
+	'forum_message_trop_long' => 'Il tuo messaggio è troppo lungo. La dimensione massima è di 20000 caratteri.',
 	'forum_ne_repondez_pas' => 'Non rispondere a questa email ma intervieni sul forum all\'indirizzo seguente:',
 	'forum_page_url' => '(Se il tuo messaggio si riferisce ad un articolo pubblicato sul Web o ad una pagina contenente maggiori informazioni, è possibile indicare di seguito il titolo della pagina ed il suo indirizzo URL.)',
-	'forum_permalink' => 'Lien permanent vers le commentaire', # NEW
+	'forum_permalink' => 'Permalink al commento',
 	'forum_poste_par' => 'Messaggio inviato da @parauteur@ in calce all\'articolo  « @titre@ ».',
-	'forum_qui_etes_vous' => '<b>Chi sei?</b> (opzionale)', # MODIF
-	'forum_saisie_texte_info' => 'Ce formulaire accepte les raccourcis SPIP <code>[-&gt;url] {{gras}} {italique} &lt;quote&gt; &lt;code&gt;</code> et le code HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Pour créer des paragraphes, laissez simplement des lignes vides.', # NEW
-	'forum_texte' => 'Testo del messaggio:', # MODIF
-	'forum_titre' => 'Titolo:', # MODIF
-	'forum_url' => 'URL:', # MODIF
+	'forum_qui_etes_vous' => 'Chi sei?',
+	'forum_saisie_texte_info' => 'Questo form accetta scorciatoie di SPIP <code>[-&gt;url] {{bold}} {italic} &lt;quote&gt; &lt;code&gt;</code> e il codice HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Per creare un paragrafo lasciate semplicemente una riga vuota.',
+	'forum_texte' => 'Testo del messaggio',
+	'forum_titre' => 'Titolo',
+	'forum_url' => 'URL',
 	'forum_valider' => 'Conferma la scelta',
-	'forum_voir_avant' => 'Vedi il messaggio prima di inviarlo', # MODIF
-	'forum_votre_email' => 'Indirizzo email:', # MODIF
-	'forum_votre_nom' => 'Nome (o pseudonimo):', # MODIF
+	'forum_voir_avant' => 'Vedi il messaggio prima di inviarlo',
+	'forum_votre_email' => 'Indirizzo email',
+	'forum_votre_nom' => 'Nome',
 	'forum_vous_enregistrer' => 'È necessario iscriversi
 per partecipare a questo forum.  Indica qui sotto l\'ID personale
 che ti è stato fornito.  Se non sei registrato, devi',
 	'forum_vous_inscrire' => 'prima iscriverti.',
 
 	// I
-	'icone_bruler_message' => 'Signaler comme Spam', # NEW
-	'icone_bruler_messages' => 'Signaler comme Spam', # NEW
-	'icone_legitimer_message' => 'Signaler comme licite', # NEW
+	'icone_bruler_message' => 'Segnala come SPAM',
+	'icone_bruler_messages' => 'Segnala come SPAM',
+	'icone_legitimer_message' => 'Segnala come illegale',
 	'icone_poster_message' => 'Invia un messaggio',
 	'icone_suivi_forum' => 'Andamento del forum pubblico: @nb_forums@ contributi',
 	'icone_suivi_forums' => 'Gestione dei forum',
 	'icone_supprimer_message' => 'Elimina il messaggio',
-	'icone_supprimer_messages' => 'Supprimer ces messages', # NEW
+	'icone_supprimer_messages' => 'Cancella questi messaggi',
 	'icone_valider_message' => 'Convalida il messaggio',
-	'icone_valider_messages' => 'Valider ces messages', # NEW
-	'icone_valider_repondre_message' => 'Valider & Répondre à ce message', # NEW
-	'info_1_message_forum' => '1 message de forum', # NEW
-	'info_activer_forum_public' => '<i>Per attivare i forum pubblici, scegli il tipo
-di moderazione predefinito:</i>', # MODIF
+	'icone_valider_messages' => 'Velida questi messaggi',
+	'icone_valider_repondre_message' => 'Conferma e rispondi a questo messaggio',
+	'info_1_message_forum' => '1 messaggio nel forum',
+	'info_activer_forum_public' => 'Per attivare i forum pubblici, scegli il tipo di moderazione predefinito:',
 	'info_appliquer_choix_moderation' => 'Questa scelta di moderazione verrà applicata:',
-	'info_config_forums_prive' => 'Nell\'area riservata del sito è possibile attivare diversi tipi di forum:', # MODIF
+	'info_config_forums_prive' => 'Nell\'area riservata del sito è possibile attivare diversi tipi di forum:',
 	'info_config_forums_prive_admin' => 'Un forum riservato agli amministratori del sito:',
 	'info_config_forums_prive_global' => 'Un forum globale, aperto a tutti i redattori:',
 	'info_config_forums_prive_objets' => 'Un forum per ogni articolo, breve, sito repertoriato, ecc.:',
@@ -92,23 +91,23 @@ I forum pubblici potranno essere autorizzati di volta in volta
 sui singoli articoli; saranno invece proibiti nelle rubriche, nelle brevi, ecc',
 	'info_envoi_forum' => 'Notifica dei forum agli autori degli articoli',
 	'info_fonctionnement_forum' => 'Funzionamento del forum:',
-	'info_forums_liees_mot' => 'Les messages de forum liés à ce mot', # NEW
-	'info_gauche_suivi_forum_2' => 'La pagina di <i>monitoraggio dei forum</i> è uno strumento di gestione del sito (e non uno spazio di discussione o di redazione). In essa sono pubblicati tutti i contributi dei forum del sito, sia quelli del sito pubblico che quelli dell\'area riservata, permettendone la gestione.', # MODIF
+	'info_forums_liees_mot' => 'Messaggi del forum collegati a questa parola chiave',
+	'info_gauche_suivi_forum_2' => 'La pagina di <i>monitoraggio dei forum</i> è uno strumento di gestione del sito e non uno spazio di discussione o di redazione. In essa sono pubblicati tutti i contributi dei forum del sito, sia quelli del sito pubblico che quelli dell\'area riservata, permettendone la gestione.',
 	'info_liens_syndiques_3' => 'forum',
 	'info_liens_syndiques_4' => 'sono',
 	'info_liens_syndiques_5' => 'forum',
 	'info_liens_syndiques_6' => 'è',
 	'info_liens_syndiques_7' => 'in attesa di convalida',
-	'info_liens_texte' => 'Lien(s) contenu(s) dans le texte du message', # NEW
-	'info_liens_titre' => 'Lien(s) contenu(s) dans le titre du message', # NEW
+	'info_liens_texte' => 'Collegamenti nel testo del messaggio',
+	'info_liens_titre' => 'Collegamenti nel titolo del messaggio',
 	'info_mode_fonctionnement_defaut_forum_public' => 'Modo di funzionamento predefinito dei forum pubblici',
-	'info_nb_messages_forum' => '@nb@ messages de forum', # NEW
+	'info_nb_messages_forum' => '@nb@ messaggi nel forum',
 	'info_option_email' => 'Quando un visitatore del sito inserisce un nuovo messaggio nel forumassociato a un articolo, gli autori di quest\'ultimo possono essereavvertiti via email. Per ogni tipo di forum, indica se desideri utilizzare quest\'opzione.',
 	'info_pas_de_forum' => 'nessun forum',
-	'info_question_visiteur_ajout_document_forum' => 'Se si desidera autorizzare i visitatori ad allegare dei documenti (immagini, musiche, ecc.) ai propri messaggi nel forum, indicare qui sotto l\'elenco delle estensioni dei documenti autorizzati per i forum (per es.: gif, jpg, png, mp3).', # MODIF
-	'info_question_visiteur_ajout_document_forum_format' => 'Se si desidera autorizzare tutti i tipi di documenti considerati affidabili da SPIP, mettere un asterisco. Per non autorizzare nulla, non indicare nulla.', # MODIF
-	'interface_formulaire' => 'Interface formulaire', # NEW
-	'interface_onglets' => 'Interface avec onglets', # NEW
+	'info_question_visiteur_ajout_document_forum' => 'Se si desidera autorizzare i visitatori ad allegare dei documenti (immagini, musiche, ecc.) ai propri messaggi nel forum, indicare qui sotto l\'elenco delle estensioni dei documenti autorizzati per i forum (per es.: gif, jpg, png, mp3).',
+	'info_question_visiteur_ajout_document_forum_format' => 'Se si desidera autorizzare tutti i tipi di documenti considerati affidabili da SPIP, mettere un asterisco. Per non autorizzare nulla lasciate il campo vuoto.',
+	'interface_formulaire' => 'Interfaccia form',
+	'interface_onglets' => 'Interfaccia a tab',
 	'item_activer_forum_administrateur' => 'Attiva il forum degli amministratori',
 	'item_config_forums_prive_global' => 'Attiva i forum dei redattori',
 	'item_config_forums_prive_objets' => 'Attiva questi forum',
@@ -119,25 +118,25 @@ sui singoli articoli; saranno invece proibiti nelle rubriche, nelle brevi, ecc',
 	// L
 	'lien_reponse_article' => 'Risposta all\'articolo',
 	'lien_reponse_breve_2' => 'Risposta alla breve',
-	'lien_reponse_message' => 'Réponse au message', # NEW
+	'lien_reponse_message' => 'Rispondi al messaggio',
 	'lien_reponse_rubrique' => 'Risposta alla rubrica',
-	'lien_reponse_site_reference' => 'Risposta al sito repertoriato:', # MODIF
+	'lien_reponse_site_reference' => 'Commenti su questo sito',
 
 	// M
-	'messages_aucun' => 'Aucun', # NEW
-	'messages_meme_auteur' => 'Tous les messages de cet auteur', # NEW
-	'messages_meme_email' => 'Tous les messages de cet email', # NEW
-	'messages_meme_ip' => 'Tous les messages de cette IP', # NEW
-	'messages_off' => 'Supprimés', # NEW
-	'messages_perso' => 'Personnels', # NEW
-	'messages_privadm' => 'Administrateurs', # NEW
-	'messages_prive' => 'Privés', # NEW
-	'messages_privoff' => 'Supprimés', # NEW
-	'messages_privrac' => 'Généraux', # NEW
-	'messages_prop' => 'Proposés', # NEW
-	'messages_publie' => 'Publiés', # NEW
-	'messages_spam' => 'Spam', # NEW
-	'messages_tous' => 'Tous', # NEW
+	'messages_aucun' => 'Nessuno',
+	'messages_meme_auteur' => 'Tutti i messaggi di questo autore',
+	'messages_meme_email' => 'Tutti i messaggi da questa email',
+	'messages_meme_ip' => 'Tutti i messaggi da questo IP',
+	'messages_off' => 'Cancellati',
+	'messages_perso' => 'Personale',
+	'messages_privadm' => 'Amministratori',
+	'messages_prive' => 'Privato',
+	'messages_privoff' => 'Cancellati',
+	'messages_privrac' => 'Generale',
+	'messages_prop' => 'Proposto',
+	'messages_publie' => 'Pubblicato',
+	'messages_spam' => 'Spam',
+	'messages_tous' => 'Tutto',
 
 	// O
 	'onglet_messages_internes' => 'Messaggi interni',
@@ -148,28 +147,28 @@ sui singoli articoli; saranno invece proibiti nelle rubriche, nelle brevi, ecc',
 	'repondre_message' => 'Rispondere al messaggio',
 
 	// S
-	'statut_off' => 'Supprimé', # NEW
+	'statut_off' => 'Cancellato',
 	'statut_original' => 'originale',
-	'statut_prop' => 'Proposé', # NEW
-	'statut_publie' => 'Publié', # NEW
-	'statut_spam' => 'Spam', # NEW
+	'statut_prop' => 'Proposto',
+	'statut_publie' => 'Pubblicato',
+	'statut_spam' => 'Spam',
 
 	// T
 	'text_article_propose_publication_forum' => 'Non esitare ad esprimere il tuo punto di vista nel forum di questo articolo (a fondo pagina).',
-	'texte_en_cours_validation' => 'Les articles, brèves, forums ci dessous sont proposés à la publication.', # NEW
+	'texte_en_cours_validation' => 'Articoli, brevi, forum sottostanti sono proposti per la pubblicazione',
 	'texte_en_cours_validation_forum' => 'Non esitare ad esprimere il tuo punto di vista utilizzando i forum ad essi dedicati.',
-	'texte_messages_publics' => 'Messages publics sur :', # NEW
+	'texte_messages_publics' => 'Messaggi pubblici su:',
 	'titre_cadre_forum_administrateur' => 'Forum privato degli amministratori',
 	'titre_cadre_forum_interne' => 'Forum interno',
 	'titre_config_forums_prive' => 'Forum dell\'area riservata',
 	'titre_forum' => 'Forum',
 	'titre_forum_suivi' => 'Monitoraggio dei forum',
 	'titre_page_forum_suivi' => 'Monitoraggio dei forum',
-	'titre_selection_action' => 'Sélection', # NEW
-	'tout_voir' => 'Voir tous les messages', # NEW
+	'titre_selection_action' => 'Selezione',
+	'tout_voir' => 'Vedi tutti i messaggi',
 
 	// V
-	'voir_messages_objet' => 'voir les messages' # NEW
+	'voir_messages_objet' => 'vedi il messaggio'
 );
 
 ?>
