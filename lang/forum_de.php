3dd48cfe4ad99531a824510a81f3d71049ea6088
salvatore <salvatore@rezo.net>
2012-05-21 12:42:11 +0000

    langues (forum)

--- lang/forum_de.php
+++ lang/forum_de.php
@@ -15,10 +15,10 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'bouton_radio_articles_tous' => 'für ausnahmslos alle Artikel.',
 	'bouton_radio_articles_tous_sauf_forum_desactive' => 'nur für Artikel mit aktiviertem Forum.',
 	'bouton_radio_enregistrement_obligatoire' => 'Obligatorische Anmeldung (Besucher müssen sich mit einer gültigen E-Mail Adresse anmelden, um Forumsbeiträge schreiben zu können).',
-	'bouton_radio_moderation_priori' => 'Vorgeschaltete Moderation (Die Benutzerkommentare werden erst nach Bestätigung durch einen Administrator sichtbar). ', # MODIF
+	'bouton_radio_moderation_priori' => 'Vorgeschaltete Moderation (Die Benutzerkommentare werden erst nach Bestätigung durch einen Administrator sichtbar). ',
 	'bouton_radio_modere_abonnement' => 'Mit Anmeldung',
-	'bouton_radio_modere_posteriori' => 'Nachträgliche Moderation', # MODIF
-	'bouton_radio_modere_priori' => 'Vorgeschaltete Moderation', # MODIF
+	'bouton_radio_modere_posteriori' => 'Nachträgliche Moderation',
+	'bouton_radio_modere_priori' => 'Vorgeschaltete Moderation',
 	'bouton_radio_publication_immediate' => 'Sofortige Veröffentlichung von Beiträgen (sie werden sofort nach dem Absenden angezeigt und können von Administratoren nachträglich gelöscht werden).',
 
 	// D
@@ -34,28 +34,28 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'forum_attention_dix_caracteres' => '<b>Achtung!</b> Ihr Beitrag ist muss mindestens 10 Zeichen lang sein.',
 	'forum_attention_message_non_poste' => 'Pozor, neposlali ste svoj príspevok!',
 	'forum_attention_trois_caracteres' => '<b>Achtung!</b> Ihr Titel muss mindestens drei Zeichen lang sein.',
-	'forum_attention_trop_caracteres' => '<b>Achtung!</b> Ihr Beitrag ist zu lang (@compte@ Zeichen). Damit er gespeichert werden kann, darf er maximal @max@ Zeichen lang sein.', # MODIF
+	'forum_attention_trop_caracteres' => '<b>Achtung!</b> Ihr Beitrag ist zu lang (@compte@ Zeichen). Damit er gespeichert werden kann, darf er maximal @max@ Zeichen lang sein.',
 	'forum_avez_selectionne' => 'Sie haben ausgewählt:',
 	'forum_cliquer_retour' => '<a href=\'@retour_forum@\'>Weiter</a>',
 	'forum_forum' => 'Forum',
-	'forum_info_modere' => 'Dieses Forum ist moderiert. Ihr Beitrag erscheint erst nach Freischaltung durch einen Administrator der Website.', # MODIF
-	'forum_lien_hyper' => '<b>Hyperlink</b> (optional)', # MODIF
-	'forum_message' => 'Votre message', # NEW
+	'forum_info_modere' => 'Dieses Forum ist moderiert. Ihr Beitrag erscheint erst nach Freischaltung durch einen Administrator der Website.',
+	'forum_lien_hyper' => 'Hyperlink',
+	'forum_message' => 'Ihr Beitrag',
 	'forum_message_definitif' => 'Beitrag fertiggestellt: Endgültig eintragen.',
-	'forum_message_trop_long' => 'Ihr Beitrag ist zu lang. Sie können maximal 20.000 Zeichen schreiben.', # MODIF
+	'forum_message_trop_long' => 'Ihr Beitrag ist zu lang. Sie können maximal 20.000 Zeichen schreiben.',
 	'forum_ne_repondez_pas' => 'Antworten Sie nicht auf diese Mail, sondern im Forum mit dieser Adresse:',
 	'forum_page_url' => '(Wenn sich Ihr Beitrag auf einen Artikel im Internet oder auf eine Seite mit Zusatzinformationen bezieht, geben Sie hier bitte den Titel der Seite und ihre Adresse bzw. URL an.)',
-	'forum_permalink' => 'Lien permanent vers le commentaire', # NEW
+	'forum_permalink' => 'Permalink des Beitrags',
 	'forum_poste_par' => 'Beitrag von @parauteur@ als Antwort auf den Artikel « @titre@ ».',
-	'forum_qui_etes_vous' => '<b>Wer sind Sie?</b> (optional)', # MODIF
-	'forum_saisie_texte_info' => 'Ce formulaire accepte les raccourcis SPIP <code>[-&gt;url] {{gras}} {italique} &lt;quote&gt; &lt;code&gt;</code> et le code HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Pour créer des paragraphes, laissez simplement des lignes vides.', # NEW
-	'forum_texte' => 'Text Ihres Beitrags:', # MODIF
-	'forum_titre' => 'Titel:', # MODIF
-	'forum_url' => 'URL:', # MODIF
+	'forum_qui_etes_vous' => 'Wer sind Sie?',
+	'forum_saisie_texte_info' => 'In diesem Formular können Sie die SPIP-Tags <code>[-&gt;url] {{gras}} {italique} &lt;quote&gt; &lt;code&gt;</code>sowie die HTML-Codes <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code> verwenden.Absätze fügen Sie mit Leerzeilen ein.',
+	'forum_texte' => 'Text Ihres Beitrags',
+	'forum_titre' => 'Titel',
+	'forum_url' => 'Ihre Website',
 	'forum_valider' => 'Auswahl bestätigen',
-	'forum_voir_avant' => 'Nachricht vor Veröffentlichung ansehen', # MODIF
-	'forum_votre_email' => 'Ihr E-Mail Adresse:', # MODIF
-	'forum_votre_nom' => 'Ihr Name (oder Pseudonym) :', # MODIF
+	'forum_voir_avant' => 'Vorschau starten',
+	'forum_votre_email' => 'Ihr E-Mail Adresse',
+	'forum_votre_nom' => 'Ihr Name',
 	'forum_vous_enregistrer' => 'Um an diesem Forum teilnehmen zu können, müssen Sie sich anmelden. Bitte tragen Sie weiter unten die Zugangsdaten ein, die Sie per Mail erhalten haben. Wenn Sie noch nicht angemeldet sind, können Sie sich',
 	'forum_vous_inscrire' => 'online anmelden.',
 
@@ -70,12 +70,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_supprimer_messages' => 'Diese Beiträge löschen',
 	'icone_valider_message' => 'Beitrag freigeben',
 	'icone_valider_messages' => 'Diese Beiträge freischalten',
-	'icone_valider_repondre_message' => 'Bestätigung; Diesen Beitrag beantworten', # MODIF
+	'icone_valider_repondre_message' => 'Diesen Beitrag veröffentlichen und beantworten',
 	'info_1_message_forum' => '1 Beitrag im Forum',
-	'info_activer_forum_public' => '<i>Um die öffentlichen Foren zu aktivieren, wählen Sie bitte
- die Moderation im Standardmodus:</i>', # MODIF
+	'info_activer_forum_public' => 'Um die öffentlichen Foren zu aktivieren, wählen Sie bitte
+ den voreingestellten Moderationmodus:',
 	'info_appliquer_choix_moderation' => 'Moderationstyp anwenden:',
-	'info_config_forums_prive' => 'Im Redaktionsbereich können Sie verschiedene Foren aktivieren:', # MODIF
+	'info_config_forums_prive' => 'Im Redaktionsbereich können Sie verschiedene Arten Forum aktivieren:',
 	'info_config_forums_prive_admin' => 'Ein Forum nur für Administratoren der Site:',
 	'info_config_forums_prive_global' => 'Ein allgemeines Forum für alle Redakteure:',
 	'info_config_forums_prive_objets' => 'Ein Forum unter jedem Artikel, Meldung, verlinkten Website, etc.:',
@@ -83,7 +83,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_envoi_forum' => 'Forumsbeiträge an Autoren der Artikel schicken',
 	'info_fonctionnement_forum' => 'Funktionsweise des Forums:',
 	'info_forums_liees_mot' => 'Beiträge mit diesem Schlagwort',
-	'info_gauche_suivi_forum_2' => 'Die Seite zum <i>Verwalten der Foren</i> hat eine administrative Funktion und dient nicht zum Diskutieren mit den Lesern. Es werden alle Einträge in öffentlichen und internen Foren gezeigt, so dass Sie diese einzeln ein- und ausschalten können.', # MODIF
+	'info_gauche_suivi_forum_2' => 'Die Seite zum <i>Verwalten der Foren</i> hat eine administrative Funktion und dient nicht zum Diskutieren mit den Lesern. Es werden alle Einträge in öffentlichen und internen Foren gezeigt, so dass Sie diese einzeln autorisieren und ablehnen können.',
 	'info_liens_syndiques_3' => 'Foren',
 	'info_liens_syndiques_4' => 'sind',
 	'info_liens_syndiques_5' => 'Forum',
@@ -95,8 +95,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_nb_messages_forum' => '@nb@ Beiträge im Forum',
 	'info_option_email' => 'Wenn ein Besucher einen Beitrag im Forum eines Artikels postet, kann SPIP ihn an den Autor des Artikels schicken. Bitte geben Sie für jeden Inhaltstyp an, ob sie diese Funktion nutzen möchten.',
 	'info_pas_de_forum' => 'kein Forum',
-	'info_question_visiteur_ajout_document_forum' => 'Wenn Sie den Besuchern gestatten, ihren Forumsbeiträgen Dokumente (Töne, Bilder, ...) beizufügen, wählen Sie bitte aus der Liste die erlaubten Formate (z.B. gif, jpg, png, mp3).', # MODIF
-	'info_question_visiteur_ajout_document_forum_format' => 'Wenn Sie alle von SPIP akzeptierten Dateitypen zulassen möchten, geben Sie bitte einen Asterisk (*) ein. Wenn Sie keine Dokumente zulassen möchten, tragen Sie keinen Wert ein.', # MODIF
+	'info_question_visiteur_ajout_document_forum' => 'Wenn Sie den Besuchern gestatten, ihren Forumsbeiträgen Dokumente (Töne, Bilder, ...) beizufügen, wählen Sie bitte aus der Liste die erlaubten Formate (z.B. gif, jpg, png, mp3).',
+	'info_question_visiteur_ajout_document_forum_format' => 'Wenn Sie alle von SPIP akzeptierten Dateitypen zulassen möchten, geben Sie bitte einen Asterisk (*) ein. Wenn Sie keine Dokumente zulassen möchten, tragen Sie keinen Wert ein.',
 	'interface_formulaire' => 'Interface als Formular',
 	'interface_onglets' => 'Interface mit Reitern',
 	'item_activer_forum_administrateur' => 'Admin-Forum aktivieren',
@@ -109,9 +109,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// L
 	'lien_reponse_article' => 'Antwort auf Artikel',
 	'lien_reponse_breve_2' => 'Antwort auf Meldung',
-	'lien_reponse_message' => 'Réponse au message', # NEW
+	'lien_reponse_message' => 'Beitrag beantworten',
 	'lien_reponse_rubrique' => 'Antwort auf Rubrik',
-	'lien_reponse_site_reference' => 'Antwort auf verlinkte Website:', # MODIF
+	'lien_reponse_site_reference' => 'Antwort auf verlinkte Website',
 
 	// M
 	'messages_aucun' => 'Kein',
@@ -146,7 +146,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// T
 	'text_article_propose_publication_forum' => 'Bitte schreiben Sie Ihren Kommentar in das Forum zu diesem Artikel (unten auf der Seite).',
-	'texte_en_cours_validation' => 'Diese Artikel, Meldungen und Forumsbeiträge sind zur Veröffentlichung vorgeschlagen', # MODIF
+	'texte_en_cours_validation' => 'Diese Artikel, Meldungen und Forumsbeiträge sind zur Veröffentlichung vorgeschlagen',
 	'texte_en_cours_validation_forum' => 'Bitte schreiben Sie Ihre Kommentare in die entsprechenden Foren.',
 	'texte_messages_publics' => 'Öffentliche Beiträge zu:',
 	'titre_cadre_forum_administrateur' => 'Administratorforum',
