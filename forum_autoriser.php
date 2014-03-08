b49567013b909651ef32c4befe3219aad44e8a0f
eric <eric@smellup.net>
2014-03-08 16:10:55 +0000

    Report de r81273. Ajout d'une autorisation générique participer aux forums privés pour un objet quelconque. Cette autorisation permet d'afficher ou non les boutons "poster un message" ou "répondre au message". Par défaut, cette autorisation est toujours vraie. Tout objet peut la surcharger.

--- forum_autoriser.php
+++ forum_autoriser.php
@@ -188,4 +188,21 @@ function autoriser_forumadmin_participer_dist($faire, $type, $id, $qui, $opt) {
 	return ($GLOBALS['meta']['forum_prive_admin'] == 'oui') && $qui['statut']=='0minirezo';
 }
 
+
+/**
+ * Autorisation de participer au forum privé d'un objet quelconque
+ *
+ * Afin de rester compatible avec l'existant cette autorisation est toujours vraie.
+ *
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+ */
+function autoriser_participerforumprive_dist($faire, $type, $id, $qui, $opt) {
+	return true;
+}
+
 ?>
