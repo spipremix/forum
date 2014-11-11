a4730424b454d5e145916868e7b87d16abdd7574
maieul <maieul@maieul.net>
2014-11-11 11:22:50 +0000

    nous disons donc : pas de raison que les ofrums de l'espace privé échappe aux règles sur la longueur minimal. Merci à b_b pour le revert du commit trop fort
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/forum@85969 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- formulaires/forum_prive.php
+++ formulaires/forum_prive.php
@@ -77,8 +77,12 @@ function formulaires_forum_prive_verifier_dist($objet, $id_objet, $id_forum, $fo
 
 	$erreurs = array();
 
-	if (strlen($texte = _request('texte')) < 10 AND $GLOBALS['meta']['forums_texte'] == 'oui')
-		$erreurs['texte'] = _T('forum:forum_attention_dix_caracteres');
+	$min_length = (defined('_FORUM_LONGUEUR_MINI') ? _FORUM_LONGUEUR_MINI : 10);
+	if (strlen($texte = _request('texte'))<$min_length
+		AND !$ajouter_mot AND $GLOBALS['meta']['forums_texte']=='oui'
+	){
+		$erreurs['texte'] = _T($min_length==10 ? 'forum:forum_attention_dix_caracteres' : 'forum:forum_attention_nb_caracteres_mini', array('min' => $min_length));
+	}
 	else if (defined('_FORUM_LONGUEUR_MAXI')
 	AND _FORUM_LONGUEUR_MAXI > 0
 	AND strlen($texte) > _FORUM_LONGUEUR_MAXI)
