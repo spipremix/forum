dc7edb2ce196fb1fa572ca28ba3daee4184c4fde
sylvain <sylvain@oliver-fr.org>
2015-02-16 04:33:38 +0000

    [Salvatore] [source:_core_/plugins/forum/lang/ forum] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/forum@87483 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lang/forum_oc_ni_mis.php
@@ -0,0 +1,159 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/forum?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_message_forum' => 'Mìnga message de forou',
+
+	// B
+	'bouton_radio_articles_futurs' => 'ai article futur unicamen (minga d’acioun sus la basa de dounada).',
+	'bouton_radio_articles_tous' => 'a toui lu article sensa echecioun',
+	'bouton_radio_articles_tous_sauf_forum_desactive' => 'a toui lu article, levat aquelu que lou forou es desactivat.',
+	'bouton_radio_enregistrement_obligatoire' => 'Registramen obligatori (lu utilisaire si devon abounà en dounant li sieu adressa e-mail denant de poudé poustà li countribucioun).',
+	'bouton_radio_moderation_priori' => 'Mouderacioun <i>a priori</i> (li
+	countribucioun aparèisson publicamen après validacioun dei
+	aministratour).',
+	'bouton_radio_modere_abonnement' => 'sus abounamen',
+	'bouton_radio_modere_posteriori' => 'mouderacioun a posteriori',
+	'bouton_radio_modere_priori' => 'mouderacioun a priori',
+	'bouton_radio_publication_immediate' => 'Publicacioun immediata dei message
+	(li countribucioun aparéisson sus lou còu que soun mandadi, lu aministratour li pouòdon
+	suprimà pi).',
+
+	// D
+	'documents_interdits_forum' => 'Doucumen prouhibat en lou forou',
+
+	// E
+	'erreur_enregistrement_message' => 'Lou vouòstre message a pas pouscut estre registrat per l’encausa d’un proublema tècnicou',
+	'extensions_autorisees' => 'Estensioun autourisadi :',
+	'extensions_autorisees_lien' => 'Veire touti li estensioun de fichié autourisadi',
+
+	// F
+	'form_pet_message_commentaire' => 'Un message, un coumentari ?',
+	'forum' => 'Forou',
+	'forum_acces_refuse' => 'Poudès plus achèdre en aquelu forou.',
+	'forum_attention_dix_caracteres' => '<b>Mèfi !</b> lou vouòstre message dèu avé au màncou dès caracter.',
+	'forum_attention_message_non_poste' => 'Mèfi, noun avès poustat lou vouòstre message !',
+	'forum_attention_nb_caracteres_mini' => '<b>Mèfi !</b> lou vouòstre message dèu countenì au màncou @min@ caracter.',
+	'forum_attention_trois_caracteres' => '<b>Mèfi !</b> lou vouòstre titre dèu avé au màncou tré caracter.',
+	'forum_attention_trop_caracteres' => '<b>Mèfi !</b> lou vouòstre message es tròu lonc (@compte@ caracter) per si poudé registrà, noun cau escoumpassà @max@ caracter.',
+	'forum_avez_selectionne' => 'Avès seleciounat :',
+	'forum_cliquer_retour' => 'Picàs <a href=\'@retour_forum@\'>aquì</a> da countinuà.',
+	'forum_envoyer' => 'Mandà',
+	'forum_forum' => 'forou',
+	'forum_info_modere' => 'Aqueu forou es mouderat a priori : la vouòstra countribucioun aparéisserà après èstre estada validada d’un aministratour dóu sit.',
+	'forum_lien_hyper' => 'Estac ipertèst',
+	'forum_message' => 'Lou vouòstre message',
+	'forum_message_definitif' => 'Message definitiéu : mandà au sit',
+	'forum_message_trop_long' => 'Lou vouòstre message es tròu lonc. La talha massimala es de 20 000 caracter.',
+	'forum_ne_repondez_pas' => 'Noun respoundès en aquel e-mail mà sus lou forou a l’adressa seguenta :',
+	'forum_page_url' => '(se lou vouòstre message si referisse a un article publicat sus lou web, o a una pàgina que fournisse mai d’infourmacioun, poudès endicàs aquì souta lou titre de la pàgina e la sieu adressa.)',
+	'forum_permalink' => 'Estac permanent vers lou coumentari',
+	'forum_poste_par' => 'Message poustat @parauteur@ a la seguida de l’article « @titre@ ».',
+	'forum_poste_par_court' => 'Message poustat @parauteur@.',
+	'forum_poste_par_generique' => 'Message poustat @parauteur@ (@objet@ « @titre@ »).',
+	'forum_qui_etes_vous' => 'Qu sias ?',
+	'forum_saisie_texte_info' => 'Aqueu fourmulari acheta li escourcha SPIP <code>[-&gt;url] {{gras}} {italique} &lt;quote&gt; &lt;code&gt;</code> e lou code HTML <code>&lt;q&gt; &lt;del&gt; &lt;ins&gt;</code>. Per creà lu paragrafe, laissàs simplamen dei ligna vuèii.',
+	'forum_texte' => 'Test dóu vouòstre message',
+	'forum_titre' => 'Titre',
+	'forum_url' => 'Lou vouòstre sit web',
+	'forum_valider' => 'Validà aqueu chausida',
+	'forum_voir_avant' => 'Previsualisà',
+	'forum_votre_email' => 'La vouòstra adressa e-mail',
+	'forum_votre_nom' => 'Lou vouòstre noum',
+	'forum_vous_enregistrer' => 'Per participà à
+  aqueu forou, devès d’en premier vous registrà. Mercì
+  d’endicà aquì souta l’identificant persounal que vous es
+  estat fournit. Se noun sias registrat, devès',
+	'forum_vous_inscrire' => 'vous inscriéure.',
+
+	// I
+	'icone_bruler_message' => 'Signalà couma Spam',
+	'icone_bruler_messages' => 'Signalà couma Spam',
+	'icone_legitimer_message' => 'Signalà couma licit',
+	'icone_poster_message' => 'Poustà un message',
+	'icone_suivi_forum' => 'Segut dóu forou public : @nb_forums@ countribucioun',
+	'icone_suivi_forums' => 'Sègre/gerà lu forou',
+	'icone_supprimer_message' => 'Suprimà aqueu message',
+	'icone_supprimer_messages' => 'Suprimà aquelu message',
+	'icone_valider_message' => 'Validà aquelu message',
+	'icone_valider_messages' => 'Validà aquelu message',
+	'icone_valider_repondre_message' => 'Validà e Respouòndre en aqueu message',
+	'info_1_message_forum' => '1 message de forou',
+	'info_activer_forum_public' => 'Per ativà lu forou public, vourguès chausì lu sieu mòdou
+ de mouderacioun predefinit :',
+	'info_appliquer_choix_moderation' => 'Aplicà aquela chausida de mouderacioun :',
+	'info_config_forums_prive' => 'En l’espaci privat dau sit, poudès ativà plusiur tìpou de forou :',
+	'info_config_forums_prive_admin' => 'Un forou reservat ai aministratour dau sit :',
+	'info_liens_syndiques_3' => 'forou',
+	'info_liens_syndiques_4' => 'soun',
+	'info_liens_syndiques_5' => 'forou',
+	'info_liens_syndiques_6' => 'es',
+	'info_liens_syndiques_7' => 'en aspèra de validacioun',
+	'info_nb_messages_forum' => '@nb@ message de forou',
+	'info_pas_de_forum' => 'mìnga forou',
+	'info_selectionner_message' => 'Seleciounà lu message :',
+	'item_activer_forum_administrateur' => 'Ativà lou forou dei aministratour',
+	'item_config_forums_prive_global' => 'Ativà lou forou dei redatour',
+	'item_config_forums_prive_objets' => 'Ativà aquelu forou',
+	'item_desactiver_forum_administrateur' => 'Desativà lou forou dei aministratour',
+	'item_non_config_forums_prive_global' => 'Desativà lou forou dei redatour',
+	'item_non_config_forums_prive_objets' => 'Desativà aquelu forou',
+
+	// L
+	'label_selectionner' => 'Seleciounà :',
+	'lien_reponse_article' => 'Respouòsta a l’article',
+	'lien_reponse_breve_2' => 'Respouòsta a la brèva',
+	'lien_reponse_message' => 'Respouòsta au message',
+	'lien_reponse_rubrique' => 'Respouòsta a la rùbrica',
+	'lien_reponse_site_reference' => 'Respouòsta au sit referençat',
+	'lien_vider_selection' => 'Vuà la selecioun',
+
+	// M
+	'messages_aucun' => 'Mìnga',
+	'messages_meme_auteur' => 'Toui lu message d’aquel autour',
+	'messages_meme_email' => 'Toui lu message d’aquel e-mail',
+	'messages_meme_ip' => 'Toui lu message d’aquela IP',
+	'messages_off' => 'Suprimat',
+	'messages_privadm' => 'Aministratour',
+	'messages_prive' => 'Privat',
+	'messages_privoff' => 'Suprimat',
+	'messages_prop' => 'Proupausat',
+	'messages_publie' => 'Publicat',
+	'messages_spam' => 'Spam',
+	'messages_tous' => 'Toui',
+
+	// O
+	'onglet_messages_internes' => 'Message interna', # RELIRE
+	'onglet_messages_publics' => 'Message public',
+	'onglet_messages_vide' => 'Message sensa tèst',
+
+	// R
+	'repondre_message' => 'Respouòndre en aquéu message',
+
+	// S
+	'statut_off' => 'Suprimat',
+	'statut_original' => 'ouriginal',
+	'statut_prop' => 'Proupausat',
+	'statut_publie' => 'Publicat',
+	'statut_spam' => 'Spam',
+
+	// T
+	'texte_messages_publics' => 'Message public sus :',
+	'titre_cadre_forum_administrateur' => 'Forou privat dei aministratour',
+	'titre_cadre_forum_interne' => 'Forou interna', # RELIRE
+	'titre_config_forums_prive' => 'Forou de l’espaci privat',
+	'titre_forum' => 'Forou',
+	'titre_selection_action' => 'Selecioun',
+	'tout_voir' => 'Veire toui lu message',
+
+	// V
+	'voir_messages_objet' => 'veire lu message'
+);
+
+?>
