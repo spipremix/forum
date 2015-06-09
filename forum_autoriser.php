32216a546f17dd6821e84cde77a94a7875f2b696
Bruno Bergot <bruno@eliaz.fr>
2015-06-09 17:32:50 +0000

    Fix https://core.spip.net/issues/3452 : rétablir la possibilité de joindre un document sur un forum
    Suite à r89155, il faut définir les autorisation autoriser_forum_associerdocuments_dist et autoriser_forum_dissocierdocuments_dist pour permettre de lier/délier un document à un message de forum. Les deux fonctions renvoient toujours true pour que cela fonctionne, voir remarques ici https://core.spip.net/issues/3452#note-4

--- forum_autoriser.php
+++ forum_autoriser.php
@@ -173,6 +173,38 @@ function autoriser_forum_autoassocierdocument_dist($faire, $type, $id, $qui, $op
 }
 
 /**
+ * Autorisation d'association de documents sur des forum
+ *
+ * Toujours
+ * 
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+ */
+function autoriser_forum_associerdocuments_dist($faire, $type, $id, $qui, $opt) {
+	return true;
+}
+
+/**
+ * Autorisation de dissociation de documents sur des forum
+ *
+ * Toujours
+ * 
+ * @param  string $faire Action demandée
+ * @param  string $type  Type d'objet sur lequel appliquer l'action
+ * @param  int    $id    Identifiant de l'objet
+ * @param  array  $qui   Description de l'auteur demandant l'autorisation
+ * @param  array  $opt   Options de cette autorisation
+ * @return bool          true s'il a le droit, false sinon
+ */
+function autoriser_forum_dissocierdocuments_dist($faire, $type, $id, $qui, $opt) {
+	return true;
+}
+
+/**
  * Autorisation de participer au forum des admins
  *
  * Il faut être administrateur (y compris restreint)
