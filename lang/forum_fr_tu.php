858c62fa204f8617f1042f55771b01429b981edd
real3t <real3t@gmail.com>
2015-07-21 03:33:34 +0000

    [Salvatore] [source:_core_/plugins/forum/lang/ forum] Export depuis http://trad.spip.net de la langue fr_tu

--- lang/forum_fr_tu.php
+++ lang/forum_fr_tu.php
@@ -29,6 +29,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// E
 	'erreur_enregistrement_message' => 'Ton message n’a pas pu être enregistré en raison d’un problème technique',
 	'extensions_autorisees' => 'Extensions autorisées :',
+	'extensions_autorisees_toutes' => 'toutes',
 
 	// F
 	'form_pet_message_commentaire' => 'Un message, un commentaire ?',
