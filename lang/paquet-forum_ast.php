a3715f9270afd0ec028d135ddde19fe420c8af3b
xuacusk8 <xuacusk8@gmail.com>
2017-11-14 17:43:39 +0000

    [Salvatore] [source:_core_/plugins/forum/lang/ paquet-forum] Export depuis http://trad.spip.net de la langue ast

---
+++ lang/paquet-forum_ast.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-forum?lang_cible=ast
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// F
+	'forum_description' => 'Foru de SPIP (priváu y públicu)',
+	'forum_slogan' => 'Alministración de foros privaos y públicos en SPIP'
+);
